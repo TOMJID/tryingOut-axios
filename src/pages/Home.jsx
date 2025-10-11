6566e09a16cfa5f1977fe6bd8469fa3860c9b726
tomjid <tomjidhuda04@gmail.com>
2025-10-11 12:41:39 +0600

    Implement search option

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,13 @@ import CocktailList from "../components/CocktailList";
 import Loading from "../components/Loading";
 function Home() {
   const { cocktails, error, loading, dispatch } = useAppContext();
-  const fetchCocktails = async () => {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const fetchCocktails = async (term) => {
     dispatch({ type: SET_LOADING, payload: true });
+
     try {
       const response = await axios.get(
-        "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${term}`
       );
       dispatch({ type: SET_COCKTAILS, payload: response.data.drinks });
     } catch (error) {
@@ -19,11 +21,33 @@ function Home() {
       dispatch({ type: SET_LOADING, payload: false });
     }
   };
+
   useEffect(() => {
-    fetchCocktails();
+    fetchCocktails("margarita");
   }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm.trim()) {
+      fetchCocktails(searchTerm.trim());
+    }
+  };
   return (
     <>
+      <form className="mb-4" onSubmit={handleSubmit}>
+        <div className="input-group">
+          <input
+            type="text"
+            className="form-control"
+            value={searchTerm}
+            placeholder="Search for your drinks"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button type="submit" className="btn btn-primary">
+            Search
+          </button>
+        </div>
+      </form>
       {loading ? <Loading /> : <CocktailList cocktails={cocktails} />}
     </>
   );
